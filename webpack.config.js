921b086e98f3996c20384c253f063e3bffa91ad6
Alex Valdetaro <avaldetaro@snapchat.com>
2022-09-06 17:42:31 -0700

    pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	mode: "development",
 	entry: "./src/index.js",
 	output: {
-		path: path.resolve(__dirname, "public"),
+		path: path.resolve(__dirname, "docs"),
 		filename: "bundle.js",
 	},
 	plugins: [
